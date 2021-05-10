003ef2dc7e0984b5443b94fe36b4f7aeed4980e3
AndrewFromTver <andrewsarkisyan@gmail.com>
2021-05-10 18:09:23 +0300

    -

--- backend/currency_api.py
+++ backend/currency_api.py
@@ -4,20 +4,19 @@ import app_config
 
 currency_api_key = app_config.currency_api_key
 
-def NewsFetch():	
-	# RU news api (country and apiKey)
+def CurrencyFetch():	
+	# USD currency api (source and apiKey)
 	query_params = {
-	'country': 'ru',
 	'access_key': currency_api_key
 	}
 	main_url = 'http://api.currencylayer.com/live'
 	# fetching data in json format
 	res = requests.get(main_url, params=query_params)
 	currency_page = res.json()
-	text_file = open('./app-data/currency.js', 'w')
+	text_file = open('./app-data/currency_usd.js', 'w')
 	text_file.write('currency = ')
 	text_file.close()
-	with open('./app-data/currency.js', 'a', encoding='utf-8') as f:
+	with open('./app-data/currency_usd.js', 'a', encoding='utf-8') as f:
 		json.dump(currency_page, f, ensure_ascii=False, indent=2)
     # getting all articles in a string article
 	article = currency_page['quotes']
@@ -26,8 +25,8 @@ def NewsFetch():
 	for a in article:		
 		# printing all trending news
 		print(a)
-		
+
 # Driver Code
 if __name__ == '__main__':
 	# function call
-	NewsFetch()
+	CurrencyFetch()
