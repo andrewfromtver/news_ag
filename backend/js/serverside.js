690277dbca3566b5f1b2ee109875aff09d74b8c6
AndrewFromTver <andrewsarkisyan@gmail.com>
2021-05-28 23:54:49 +0300

    -

--- backend/js/serverside.js
+++ backend/js/serverside.js
@@ -3,60 +3,94 @@ var exec = require('child_process').exec
 var url = require("url")
 
 function telegramApi(request, response) {
-    var params = url.parse(request.url,true).query;
-    exec("python3 /backend/telegram_api.py " + params.name + ' ' + params.email + ' ' + params.msg)
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Serverside request to Telegram API processed successfully');
+    var params = url.parse(request.url,true).query
+    if (params.name && params.email && params.msg) {
+        exec("python3 /backend/telegram_api.py " + params.name + ' ' + params.email + ' ' + params.msg)
+        response.setHeader('Access-Control-Allow-Origin', '*')
+        response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept')
+        response.writeHead(200, {'Content-Type': 'text/plain'})
+        response.end('Serverside request to Telegram API processed successfully')
+    }
+    else {
+        response.writeHead(404, {'Content-Type': 'text/plain; charset=utf-8'})
+        response.end('404')
+        console.log('Wrong request to Telegram API')
+    }
 }
-
 function owmApiWeather(request, response) {
-    var params = url.parse(request.url,true).query;
-    exec("python3 /backend/weather_api.py " + params.lat + ' ' + params.lon, (error, stdout, stderr) => {
-        response.setHeader('Access-Control-Allow-Origin', '*');
-        response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-        response.writeHead(200, {'Content-Type': 'json'});
-        if (stdout) {
-            response.end(stdout);
-        }
-        else {
-            response.end('Serverside request to OWM weather API processed with errors, please try again later.');
-        }
-    })
+    var params = url.parse(request.url,true).query
+    if (params.lat && params.lon) {
+        exec("python3 /backend/weather_api.py " + params.lat + ' ' + params.lon, (error, stdout, stderr) => {
+            response.setHeader('Access-Control-Allow-Origin', '*')
+            response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept')
+            response.writeHead(200, {'Content-Type': 'json'})
+            if (stdout) {
+                response.end(stdout)
+            }
+            else {
+                response.end('Serverside request to OWM weather API processed with errors, please try again later.')
+            }
+        })
+    }
+    else {
+        response.writeHead(404, {'Content-Type': 'text/plain; charset=utf-8'})
+        response.end('404')
+        console.log('Wrong request to OWM API')
+    }
 }
-
 function owmApiForecast(request, response) {
-    var params = url.parse(request.url,true).query;
-    exec("python3 /backend/forecast_api.py " + params.lat + ' ' + params.lon, (error, stdout, stderr) => {
-        response.setHeader('Access-Control-Allow-Origin', '*');
-        response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-        response.writeHead(200, {'Content-Type': 'json'});
-        if (stdout) {
-            response.end(stdout);
-        }
-        else {
-            response.end('Serverside request to OWM forecast API processed with errors, please try again later.');
-        }
-    })
+    var params = url.parse(request.url,true).query
+    if (params.lat && params.lon) {
+        exec("python3 /backend/forecast_api.py " + params.lat + ' ' + params.lon, (error, stdout, stderr) => {
+            response.setHeader('Access-Control-Allow-Origin', '*')
+            response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept')
+            response.writeHead(200, {'Content-Type': 'json'})
+            if (stdout) {
+                response.end(stdout)
+            }
+            else {
+                response.end('Serverside request to OWM forecast API processed with errors, please try again later.')
+            }
+        })
+    }
+    else {
+        response.writeHead(404, {'Content-Type': 'text/plain; charset=utf-8'})
+        response.end('404')
+        console.log('Wrong request to OWM API')
+    }
 }
-
 function googleTranslateApi(request, response) {
-    var params = url.parse(request.url,true).query;
-    exec("python3 /backend/translate_api.py " + params.query + ' ' + params.lang, (error, stdout, stderr) => {
-        response.setHeader('Access-Control-Allow-Origin', '*');
-        response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-        response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-        if (stdout) {
-            response.end(stdout);
-        }
-        else {
-            response.end('Google translate API not available, please try again later ...');
-        }
-    })
+    var params = url.parse(request.url,true).query
+    if (params.query && params.lang) {
+        var filteredQuery = params.query.replace('"',"'")
+        exec('python3 /backend/translate_api.py "' + filteredQuery + '" ' + params.lang, (error, stdout, stderr) => {
+            response.setHeader('Access-Control-Allow-Origin', '*')
+            response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept')
+            response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'})
+            if (stdout) {
+                response.end(stdout)
+            }
+            else {
+                response.end('Google translate API not available, please try again later ...')
+            }
+        })
+    }
+    else {
+        response.writeHead(404, {'Content-Type': 'text/plain; charset=utf-8'})
+        response.end('404')
+        console.log('Wrong request to Google translate API')
+    }
+}
+function initServer() {
+    try {
+        http.createServer(telegramApi).listen(8000)
+        http.createServer(owmApiWeather).listen(8100)
+        http.createServer(owmApiForecast).listen(8200)
+        http.createServer(googleTranslateApi).listen(8300)
+    } catch (error) {
+        initServer()
+        console.log(error)
+    }
 }
 
-http.createServer(telegramApi).listen(8000)
-http.createServer(owmApiWeather).listen(8100)
-http.createServer(owmApiForecast).listen(8200)
-http.createServer(googleTranslateApi).listen(8300)
+initServer()
