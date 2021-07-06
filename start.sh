78659e656fe3cb35f87007153f8691235ac2ddd9
Andranik Sarkisyan <andrewsarkisyan@gmail.com>
2021-07-06 22:34:51 +0300

    -

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# switch to project drectory
+cd ~/NEWS-AG-PRODUCTION
+
+# build docker container
+docker build -t news-ag:production . > ./build.log
+
+# start production container & forward ports to host
+docker run --rm -it \
+	-p 80:80 \
+	-p 8000:8000 \
+	-p 8100:8100 \
+	-p 8200:8200 \
+	-p 8300:8300 \
+	-p 8400:8400 \
+	-p 8500:8500 \
+	news-ag:production
